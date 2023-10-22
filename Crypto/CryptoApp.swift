e9f38507e67a173bacccd615259c87db267048d7
Ibrohim Begaliev <81876891+ibegaliev@users.noreply.github.com>
2023-10-22 13:02:49 +0500

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by ibrohim begaliev on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
